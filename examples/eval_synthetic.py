2cb54a556490f1cd9d1d8d70640bed7041d2ac48
Cédric Allain <CedricAllainEnsae@users.noreply.github.com>
2021-10-11 14:26:43 +0200

    delete old eval scripts and rename actual versions

--- examples/eval_synthetic.py
+++ examples/eval_synthetic.py
@@ -1,8 +1,12 @@
 """
-Plot figures of results on synthetic data
-(figures 2, 3, A.1 and A.2 in the paper)
+Evaluate DriPP in the multivariate (i.e., multiple drivers) case
 """
 
+# %%
+import pandas as pd
+from dripp.trunc_norm_kernel.em import compute_nexts
+from dripp.trunc_norm_kernel.optim import initialize
+from dripp.trunc_norm_kernel.model import TruncNormKernel, Intensity
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -11,6 +15,13 @@ from dripp.experiments.run_multiple_em_on_synthetic import \
     run_multiple_em_on_synthetic
 from dripp.config import SAVE_RESULTS_PATH
 from dripp.trunc_norm_kernel.model import TruncNormKernel
+from dripp.trunc_norm_kernel.simu import simulate_data
+from dripp.trunc_norm_kernel.optim import em_truncated_norm
+
+
+SAVE_RESULTS_PATH /= 'results_sythetic'
+if not SAVE_RESULTS_PATH.exists():
+    SAVE_RESULTS_PATH.mkdir(parents=True)
 
 # ===================================================
 # PLOT PARAMETERS
@@ -26,89 +37,84 @@ plt.rcParams.update({
 figsize = (5.5, 2)
 cmap = 'viridis_r'
 
-N_JOBS = 40  # number of jobs to run in parallel. To adjust based on machine
-
+# %%
 # ===================================================
 # DATA SIMULATION PARAMETERS
 # ===================================================
 
+
+N_DRIVERS = 2
+N_JOBS = 50
+SFREQ = 1000
+
 # default parameters for data simulation
 simu_params = {'lower': 30e-3, 'upper': 800e-3,
-               'baseline': 0.8, 'alpha': 0.8, 'm': 200e-3,
-               'isi': 1}
+               #    'baseline': 0.5,
+               'baseline': 0.8,
+               'alpha': [0.8, 0.8],
+               #    'm': [200e-3, 400e-3],
+               #    'm': [300e-3, 300e-3],
+               'm': [300e-3, 300e-3],
+               'sigma': [0.2, 0.05],
+               'isi': [1.2, 1.2]
+               }
 
 # parameters to vary for data simulation
 simu_params_to_vary = {
-    'sigma': [0.05, 0.2],  # "sharp" and "wide" kernel scenarios
-    'seed': list(range(50)),
-    'n_tasks': [0.2, 0.5, 0.8]
+    # 'seed': list(range(30)),
+    'seed': list(range(20)),
+    'n_tasks': [0.1, 0.3, 0.6]
+    # 'n_tasks': [0.3],
+    # 'seed': list(range(8)),
+    # 'n_tasks': [0.4]
 }
 
-# ===================================================
-# EM PARAMETERS
-# ===================================================
+# assert np.max(simu_params_to_vary['n_tasks']) * N_DRIVERS <= 1
+
 
 # default parameters for EM computation
+N_ITER = 50
 em_params = {'lower': simu_params['lower'], 'upper': simu_params['upper'],
-             'initializer': 'smart_start', 'n_iter': 200, 'alpha_pos': True,
+             'initializer': 'smart_start', 'n_iter': N_ITER, 'alpha_pos': True,
              'verbose': False}
 
 # parameters to vary for EM computation
-em_params_to_vary = {'T': np.logspace(2, 5, num=10).astype(int)}
+em_params_to_vary = {'T': np.logspace(2, 4, num=7).astype(int)}
+# em_params_to_vary = {'T': np.array([10_000])}
+T_max = em_params_to_vary['T'].max()
 
-# ===================================================
-# RUN EM ON MULTIPLE COMBINATIONS
-# ===================================================
-
-# run RM
 df_res = run_multiple_em_on_synthetic(
     simu_params, simu_params_to_vary, em_params, em_params_to_vary,
-    sfreq=1000, n_jobs=N_JOBS)
+    sfreq=SFREQ, n_jobs=N_JOBS, n_drivers=N_DRIVERS, save_results=True)
 
-# ===================================================
-# PLOT FIGURES
-# ===================================================
-
-comb1 = {'sigma': 0.2, **simu_params}  # "wide" kernel scenario
-comb2 = {'sigma': 0.05, **simu_params}  # "sharp" kernel scenario
-combs = [comb1, comb2]
-
-# ------ PLOT KERNEL RETRIEVAL ------
+# %% ------ PLOT KERNEL RETRIEVAL ------
 # (Figure 2 in paper)
 
-T = 1000
-n_tasks = 0.5
+T = em_params_to_vary['T'][-1]
+n_tasks = simu_params_to_vary['n_tasks'][-1]
 
 fig, axes = plt.subplots(1, 2, figsize=figsize)
 
 start, stop = 0, 1
 xx = np.linspace(start, stop, (stop - start) * 300)
 
-for i, this_comb in enumerate(combs):
-    # filter resulsts dataframe
-    df_comb = df_res[(df_res['T'] == T) &
-                     (df_res['n_tasks'] == n_tasks) &
-                     (df_res['lower'] == this_comb['lower']) &
-                     (df_res['upper'] == this_comb['upper']) &
-                     (df_res['baseline'] == this_comb['baseline']) &
-                     (df_res['alpha'] == this_comb['alpha']) &
-                     (df_res['m'] == this_comb['m']) &
-                     (df_res['sigma'] == this_comb['sigma'])]
-
+for i in range(N_DRIVERS):
     # define true kernel
     kernel_true = TruncNormKernel(
-        this_comb['lower'], this_comb['upper'],
-        this_comb['m'], this_comb['sigma'])
+        simu_params['lower'], simu_params['upper'],
+        simu_params['m'][i], simu_params['sigma'][i])
 
-    yy = this_comb['baseline'] + kernel_true.eval(xx)
+    yy = simu_params['baseline'] + kernel_true.eval(xx)
     axes[i].plot(xx, yy, label='Ground truth', color='black', linestyle='--')
 
     # plot a few estimated kernels
     for seed in range(8):
-        sub_df = df_comb[df_comb['seed'] == seed]
-        kernel = TruncNormKernel(this_comb['lower'], this_comb['upper'],
-                                 sub_df['m_hat'].values[0],
-                                 sub_df['sigma_hat'].values[0])
+        sub_df = df_res[(df_res['T'] == T) &
+                        (df_res['n_tasks'] == n_tasks) &
+                        (df_res['seed'] == seed)]
+        kernel = TruncNormKernel(simu_params['lower'], simu_params['upper'],
+                                 list(sub_df['m_hat'])[0][i],
+                                 list(sub_df['sigma_hat'])[0][i])
 
         yy = sub_df['baseline_hat'].values[0] + kernel.eval(xx)
 
@@ -120,18 +126,21 @@ for i, this_comb in enumerate(combs):
 
     axes[i].set_xlim(0, stop)
     axes[i].set_xlabel('Time (s)', fontsize=fontsize)
-    title = rf"$\mu={this_comb['baseline']}$, "
-    title += rf" $\alpha={this_comb['alpha']}$, "
-    title += rf" $m={this_comb['m']}$, "
-    title += rf" $\sigma={this_comb['sigma']}$"
+    title = rf"$\mu={simu_params['baseline']}$, "
+    title += rf" $\alpha={simu_params['alpha'][i]}$, "
+    title += rf" $m={simu_params['m'][i]}$, "
+    title += rf" $\sigma={simu_params['sigma'][i]}$"
     axes[i].legend(fontsize=fontsize)
     axes[i].set_title(title, fontsize=fontsize)
 
 plt.tight_layout()
-plt.savefig(SAVE_RESULTS_PATH / 'fig2.pdf', dpi=300)
+fig_name = 'fig2_multi_%i_%s_n_iter_%i.pdf' % (
+    T, str(n_tasks).replace('.', '_'), N_ITER)
+plt.savefig(SAVE_RESULTS_PATH / fig_name, dpi=300)
+plt.savefig(SAVE_RESULTS_PATH / (fig_name.replace('pdf', 'png')), dpi=300)
 plt.show()
 plt.close()
-
+# %%
 # ------ PLOT MEAN/STD OF THE RELATIVE NORM ------
 # (Figure 3 and A.1 in paper)
 
@@ -139,42 +148,36 @@ list_df_mean = []
 list_df_std = []
 list_title = []
 
-for i, this_comb in enumerate(combs):
-    # filter resulsts dataframe
-    df_comb = df_res[(df_res['lower'] == this_comb['lower']) &
-                     (df_res['upper'] == this_comb['upper']) &
-                     (df_res['baseline'] == this_comb['baseline']) &
-                     (df_res['alpha'] == this_comb['alpha']) &
-                     (df_res['m'] == this_comb['m']) &
-                     (df_res['sigma'] == this_comb['sigma'])]
+n_tasks_str = r'$n_t$'
+# n_tasks_str = 'n_t'
+df_temp = df_res.rename({'n_tasks': n_tasks_str}, axis=1)
 
-    n_tasks_str = r'$n_t$'
-    df_comb = df_comb.rename({'n_tasks': r'$n_t$'}, axis=1)
-
-    # mean
-    df_mean = df_comb[['T', n_tasks_str, 'infinite_norm_of_diff_rel']].groupby(
+for p in range(N_DRIVERS):
+    columns = ['T', n_tasks_str, 'infinite_norm_of_diff_rel_kernel_%i' % p]
+    df_mean = df_temp[columns].groupby(
         ['T', n_tasks_str]).mean().unstack()
     df_mean.columns = df_mean.columns.droplevel()
+    # df_mean.plot(logy=True, logx=True, cmap=cmap)
     list_df_mean.append(df_mean)
 
-    # std
-    df_std = df_comb[['T', n_tasks_str, 'infinite_norm_of_diff_rel']].groupby(
+    df_std = df_temp[columns].groupby(
         ['T', n_tasks_str]).std().unstack()
     df_std.columns = df_std.columns.droplevel()
     list_df_std.append(df_std)
 
     # title
-    title = rf"$\mu={this_comb['baseline']}$, "
-    title += rf" $\alpha={this_comb['alpha']}$, "
-    title += rf" $m={this_comb['m']}$, "
-    title += rf" $\sigma={this_comb['sigma']}$"
+    title = rf"$\mu={simu_params['baseline']}$, "
+    title += rf" $\alpha={simu_params['alpha'][p]}$, "
+    title += rf" $m={simu_params['m'][p]}$, "
+    title += rf" $\sigma={simu_params['sigma'][p]}$"
     list_title.append(title)
 
 
-# plot mean (Figure 3 in paper)
-fig, axes = plt.subplots(1, 2, sharey=True, sharex=True, figsize=figsize)
+# plot mean
+fig, axes = plt.subplots(1, N_DRIVERS, sharey=True,
+                         sharex=True, figsize=figsize)
 
-for ii in range(2):
+for ii in range(N_DRIVERS):
     ax = axes[ii]
     list_df_mean[ii].plot(logy=True, logx=True, cmap=cmap, ax=ax)
     ax.set_xlabel(r"$T$ (s)", fontsize=fontsize, labelpad=0)
@@ -184,19 +187,24 @@ for ii in range(2):
 
 for ax in axes.ravel():
     ax.legend([], frameon=False)
-    ax.set_xlim(df_comb['T'].min(), df_comb['T'].max())
+    ax.set_xlim(em_params_to_vary['T'].min(), em_params_to_vary['T'].max())
 
 axes[1].legend(ncol=1, handlelength=1, fontsize=fontsize)
 
 plt.tight_layout()
-plt.savefig(SAVE_RESULTS_PATH / 'fig3_mean.pdf', dpi=300, bbox_inches='tight')
+fig_name = 'fig3_mean_multi_%i_sfreq_%i' % (T_max, SFREQ)
+plt.savefig(SAVE_RESULTS_PATH / (fig_name + '.pdf'),
+            dpi=300, bbox_inches='tight')
+plt.savefig(SAVE_RESULTS_PATH / (fig_name + '.png'),
+            dpi=300, bbox_inches='tight')
 plt.show()
 plt.close()
 
-# plot std (Figure A.1 in paper)
-fig, axes = plt.subplots(1, 2, sharey=True, sharex=True, figsize=figsize)
+# plot std
+fig, axes = plt.subplots(1, N_DRIVERS, sharey=True,
+                         sharex=True, figsize=figsize)
 
-for ii in range(2):
+for ii in range(N_DRIVERS):
     ax = axes[ii]
     list_df_std[ii].plot(logy=True, logx=True, cmap=cmap, ax=ax)
     ax.set_xlabel(r"$T$ (s)", fontsize=fontsize, labelpad=0)
@@ -206,14 +214,19 @@ for ii in range(2):
 
 for ax in axes.ravel():
     ax.legend([], frameon=False)
-    ax.set_xlim(df_comb['T'].min(), df_comb['T'].max())
+    ax.set_xlim(em_params_to_vary['T'].min(), em_params_to_vary['T'].max())
 
 axes[1].legend(ncol=1, handlelength=1, fontsize=fontsize)
 
 plt.tight_layout()
-plt.savefig(SAVE_RESULTS_PATH / 'fig3_std.pdf', dpi=300, bbox_inches='tight')
+fig_name = fig_name.replace('mean', 'std')
+plt.savefig(SAVE_RESULTS_PATH / (fig_name + '.pdf'),
+            dpi=300, bbox_inches='tight')
+plt.savefig(SAVE_RESULTS_PATH / (fig_name + '.png'),
+            dpi=300, bbox_inches='tight')
 plt.show()
 plt.close()
+
 
 # ------ PLOT COMPUTATION TIME OF THE RELATIVE NORM ------
 # (Figure A.2 in paper)
@@ -222,12 +235,12 @@ plt.figure(figsize=figsize)
 
 ax = sns.lineplot(data=df_res, x="T", y="comput_time",
                   markers=["."], estimator='mean', ci=95)
-ax.set_xlim(df_comb['T'].min(), df_comb['T'].max())
+ax.set_xlim(em_params_to_vary['T'].min(), em_params_to_vary['T'].max())
 ax.set_xlabel(r"$T$ (s)", fontsize=fontsize)
 ax.set_ylabel('CPU time (s)', fontsize=fontsize)
 ax.set_xscale('log')
 
 plt.tight_layout()
-plt.savefig(SAVE_RESULTS_PATH / 'fig3_computation_time.pdf', dpi=300)
+plt.savefig(SAVE_RESULTS_PATH / 'fig3_computation_time_multi.pdf', dpi=300)
 plt.show()
 plt.close()
